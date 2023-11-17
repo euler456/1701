15cd9bdf519260a25f9fe20d4d23cac6643050e9
euler456 <euler5926@gmail.com>
2023-11-18 00:18:35 +1000

    complete doaramon

--- P5Task-main/js/sketch.js
+++ P5Task-main/js/sketch.js
@@ -5,15 +5,59 @@ function setup() {
   
 function draw() {
     background(255);
-
-    
-    // Doraemon's body
-    fill('#0294A5');
-    rect(125, 180, 150, 150,30);
     //foot
     fill(255);
     ellipse(240, 325, 80, 50);
     ellipse(160, 325, 80, 50);
+    //left hand
+    fill('#0294A5');
+    ellipse(145, 230, 70, 80);
+    //tail
+    strokeWeight(3);
+    line(140, 280, 120, 280);
+    strokeWeight(1);
+
+    //red spot
+    fill(255, 0, 0);
+    ellipse(110, 280, 30, 30);
+    
+    // Doraemon's body
+    fill('#0294A5');
+    beginShape();
+    curveVertex(140, 200);
+    curveVertex(140, 320);
+    curveVertex(140, 200);
+    curveVertex(260, 200);
+    curveVertex(260, 310);
+    curveVertex(220, 320);
+    curveVertex(210, 300);
+    curveVertex(190, 300);
+    curveVertex(180, 320);
+    curveVertex(140, 320);
+    curveVertex(140, 320);
+    endShape();
+    //right hand
+    beginShape();
+    curveVertex(260, 190);
+    curveVertex(255, 190);
+    curveVertex(300, 160);
+    curveVertex(310, 180);
+    curveVertex(270, 230);
+    curveVertex(270, 230);
+    endShape();
+    //finger
+    fill(255);
+    ellipse(320, 160, 40, 40);
+    //pocket
+    fill(255);
+    ellipse(200, 240, 90, 90);
+    let x = 200; 
+    let y = 240; 
+    let radius = 30; 
+    let startAngle = 0;
+    let endAngle = PI;
+    arc(x, y, radius * 2, radius * 2, startAngle, endAngle);
+    line(170,240,230,240)
     // Doraemon's face
     fill('#0294A5'); 
     ellipse(200, 100, 200, 200);
@@ -50,9 +94,10 @@ function draw() {
     line(230, 90, 300, 60);
     line(230, 100, 300, 100);
     line(230, 110, 300, 140);
+
     //red 
     fill(255, 0, 0);
-    rect(125, 190, 150, 10,30);
+    rect(135, 188, 125, 10,30);
     // bell
     fill('#FFD100'); // Yellow color for Doraemon's bell
     ellipse(200, 210, 30, 30);
@@ -60,6 +105,5 @@ function draw() {
     fill(0)
     rect(197, 215, 7, 10);
 
-    
   }
   
